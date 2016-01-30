910fb3da1be5abeb446c3e27587331d986317f54
Haritz Puerto <haritzpuerto94@gmail.com>
2016-01-30 16:21:11 +0100

    Added Natural Semantics for Proc

--- Proc/ProcNaturalSemantics.hs
+++ Proc/ProcNaturalSemantics.hs
@@ -41,11 +41,15 @@ type Store = Loc -> Z
 
 -- update a variable environment with a new binding envV [x -> l]
 updateV :: EnvVar -> Var -> Loc -> EnvVar
-updateV envV x l = undefined
+updateV envV x l  y 
+	| x == y = l
+	| otherwise = envV y
 
 -- update a store with a new binding sto [l -> v]
 updateS :: Store -> Loc -> Z -> Store
-updateS sto l v = undefined
+updateS sto l v l'
+	| l == l' = v
+	| otherwise = sto l'
 
 -- variable declaration configurations
 
@@ -57,10 +61,15 @@ nsDecV :: ConfigD -> ConfigD
 -- | Exercise 1.2
 
 -- var x := a
-nsDecV (InterD (Dec x a decs) envV store) = undefined
+nsDecV (InterD (Dec x a decs) envV store) = nsDecV (InterD decs envV' sto'')
+	where
+		l = store next
+		envV' = updateV envV x l 
+		sto' = updateS store l (aVal a (store . envV))
+		sto'' = updateS sto' next (new l)
 
 -- epsilon
-nsDecV (InterD EndDec envV store) = undefined
+nsDecV (InterD EndDec envV store) = FinalD envV store
 
 ----------------------------------------------------------------------
 -- Procedure Declarations
@@ -77,15 +86,17 @@ data EnvProc = EnvP Pname Stm EnvVar EnvProc EnvProc
 
 -- update the procedure environment
 updP :: DecProc -> EnvVar -> EnvProc -> EnvProc
-updP (Proc p s procs) envV envP = undefined
-updP EndProc envV envP = undefined
+updP (Proc p s procs) envV envP = updP procs envV (EnvP p s envV envP envP)
+updP EndProc envV envP = envP
 
 -- | Exercise 2.2
 
 -- lookup procedure p
 envProc :: EnvProc -> Pname -> (Stm, EnvVar, EnvProc)
-envProc (EnvP q s envV envP envs) p = undefined
-envProc EmptyEnvProc p = undefined
+envProc (EnvP q s envV envP envs) p 
+	| p == q = (s, envV, envP)
+	| otherwise = envProc envs p
+envProc EmptyEnvProc p = error "undefined procedure"
 
 -- representation of configurations for Proc
 
@@ -100,41 +111,77 @@ nsStm :: EnvVar -> EnvProc -> Config -> Config
 
 -- x := a
 
-nsStm envV envP (Inter (Ass x a) sto) = undefined
+nsStm envV envP (Inter (Ass x a) sto) = Final sto'
+	where
+		sto' = updateS sto l v
+		l = envV x
+		v = aVal a (sto . envV)
 
 
 -- skip
 
-nsStm envV envP (Inter Skip sto) = undefined
+nsStm envV envP (Inter Skip sto) = Final sto
 
 
 -- s1; s2
 
-nsStm envV envP (Inter (Comp ss1 ss2) sto) = undefined
+nsStm envV envP (Inter (Comp ss1 ss2) sto) = Final sto''
+	where
+		Final sto' = nsStm envV envP (Inter ss1 sto)
+		Final sto'' = nsStm envV envP (Inter ss2 sto')
 
 -- if b then s1 else s2
 
-nsStm envV envP (Inter (If b s1 s2) sto) = undefined
+nsStm envV envP (Inter (If b s1 s2) sto) 
+	| bVal b (sto . envV) = Final sto1
+	| otherwise = Final sto2
+	where
+		Final sto1 = nsStm envV envP (Inter s1 sto)
+		Final sto2 = nsStm envV envP (Inter s2 sto)
 
 -- while b do s
 
-nsStm envV envP (Inter (While b s) sto) = undefined
+nsStm envV envP (Inter (While b s) sto)
+	| bVal b (sto . envV) = Final sto''
+ 	| otherwise = Final sto
+ 		where
+ 			Final sto' = nsStm envV envP (Inter s sto)
+ 			Final sto'' = nsStm envV envP (Inter (While b s) sto')
 
 
-nsStm envV envP (Inter (Block vars procs s) sto) = undefined
+nsStm envV envP (Inter (Block vars procs s) sto) = Final sto''
+	where
+		FinalD envV' sto' = nsDecV (InterD vars envV sto)
+		envP' = updP procs  envV' envP
+		Final sto'' = nsStm envV' envP' (Inter s sto')
 
 -- non-recursive procedure call
 {-
-nsStm envV envP (Inter (Call p) sto) = undefined
-
-
+nsStm envV envP (Inter (Call p) sto) = Final sto'
+	where
+		Final sto' = nsStm envV' envP' (Inter s sto)
+		(s, envV', envP') = envProc envP p 
 -}
 
+
+
+
 -- recursive procedure call
-nsStm envV envP (Inter (Call p) sto) = undefined
+nsStm envV envP (Inter (Call p) sto) = Final sto'
+	where
+		Final sto' = nsStm envV' envP'' (Inter s sto)
+		(s, envV', envP') = envProc envP p
+		envP'' = updP (Proc p s EndProc)  envV' envP'
 
 -- | Exercise 3.2
 
 sNs :: Stm -> Store -> Store
-sNs s sto = undefined
+sNs s sto = sto'
+	where
+		Final sto' = nsStm emptyEnvV emptyEnvP (Inter s sto)
 
+emptyEnvV :: EnvVar
+emptyEnvV x = error $ "undefined variable " ++ x
+
+emptyEnvP :: EnvProc
+emptyEnvP = EmptyEnvProc
